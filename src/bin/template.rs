d1c9559c07b3368aef9472968775d51a0576312e
Stefan Seemayer <stefan@seemayer.de>
2020-12-15 14:27:03 +0100

    day 01 part 1 and 2

---
+++ src/bin/template.rs
@@ -0,0 +1,23 @@
+use snafu::{ResultExt, Snafu};
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("I/O error: {}", source))]
+    Io { source: std::io::Error },
+}
+
+fn main() -> Result<()> {
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() -> Result<()> {
+        Ok(())
+    }
+}
